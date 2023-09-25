2275c6663e4b4272ad1979ff0b1321622fcb4ed3
IgnaceB <ignacebernard@hotmail.com>
2023-09-25 14:14:12 +0200

    update allowed host

--- tinderLike/settings.py
+++ tinderLike/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-0nqa+hin*mc(&z=tg$i%!n8a$w(jxol*x2)$ms06he3=e)l6g0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tinderlike-app-17303f5e385a.herokuapp.com']
 
 
 # Application definition

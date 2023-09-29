5f38220747d6c8272155dec85cd49886c5e6a56f
IgnaceB <ignacebernard@hotmail.com>
2023-09-29 15:00:17 +0200

    try on static whitenoise heroky

--- tinderLike/settings.py
+++ tinderLike/settings.py
@@ -80,6 +80,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     # "whitenoise.middleware.WhiteNoiseMiddleware",
 
 ]
@@ -167,6 +168,8 @@ STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
